3edd567c28aeca6054ad9a3ba326794083e5e99a
Steven Tejeda <steventejeda@gmail.com>
2021-06-19 08:00:59 -0400

    solved problem

---
+++ twoSum.js
@@ -0,0 +1,21 @@
+var twoSum = function(nums, target) {
+    let numberIndex = new Map ();
+      let result = [];
+      
+      for(let i = 0; i < nums.length; i++) { 
+          let num = nums[i];
+          let neededNum = target - num;
+          
+          if (numberIndex.has(neededNum)) { 
+              result[0] = numberIndex.get(neededNum);
+              result[1] = i;
+              
+              return result
+          }
+          numberIndex.set(num, i);
+      }
+      
+      
+      return result;
+      
+  };
