05b8fa204423d7881aab0b1f775ffa789d9bbb7c
Steven Tejeda <steventejeda@gmail.com>
2021-07-11 21:03:19 -0400

    solved problem

--- reverseLinked.js
+++ reverseLinked.js
@@ -1,20 +1,14 @@
-// O(n) time & O(1) space
-function reverse(head) {
-    let node = head,
-        previous,
-        temp;
-  
-    while (node) {
-      // save next before node.next is overwritten
-      temp = node.next;
-  
-      // reverse pointer
-      node.next = previous;
-  
-      // step forward in the list
-      previous = node;
-      node = temp;
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    let nextTemp = null;
+
+    while(curr!= null) {
+        nextTemp = curr.next; // As I explained earlier, I save the next pointer in the temp variable.
+        curr.next = prev;  // Then I reverse the pointer of the current node to its previous node.
+        prev = curr;  //  The previous node becomes the node we are currently at.
+        curr = nextTemp;  
+// And the current nodes becomes the ext node we saved earlier. And we keep iterating.
     }
-  
-    return previous;
-  }
+    return prev // At the end, our previous node will be the head node of the new list. 
+};
