3252e4e8e7018367a1e510d3af075f571f4ceba3
Steven Tejeda <steventejeda@gmail.com>
2021-07-11 21:12:06 -0400

    solved problem

--- reverseLinked.js
+++ reverseLinked.js
@@ -1,14 +1,21 @@
 var reverseList = function(head) {
-    let prev = null;
-    let curr = head;
-    let nextTemp = null;
-
-    while(curr!= null) {
-        nextTemp = curr.next; // As I explained earlier, I save the next pointer in the temp variable.
-        curr.next = prev;  // Then I reverse the pointer of the current node to its previous node.
-        prev = curr;  //  The previous node becomes the node we are currently at.
-        curr = nextTemp;  
-// And the current nodes becomes the ext node we saved earlier. And we keep iterating.
-    }
-    return prev // At the end, our previous node will be the head node of the new list. 
-};
+    //Since this is a singly linked list, a node does not have reference to it's previous node. A pointer is needed to store a previous element of the node beforehand
+        let previous = null;
+    //To identify the current node
+        let current = head;
+    //to store the next node before changing the reference. If the 
+    //current value changes without saving it, the next pointer in //iteration is going to be lost.
+        let nextTemp = null;
+        while(current!= null) {
+    //save the next pointer in the temp variable.
+            nextTemp = current.next;
+    //reverse the pointer of the current node to its previous node.
+            current.next = previous;
+    //  The previous node becomes the node that the pointer is currently at.
+            previous = current;  
+    // And the current nodes becomes the next node that was saved earlier.
+            current = nextTemp; 
+        }
+    // At the end, the previous node will be the head node of the new list.
+        return previous
+    };
