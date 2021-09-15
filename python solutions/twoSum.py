175727fc53c13207b48d12f5cab4a4acc3855c47
steventejeda <steventejeda@gmail.com>
2021-09-14 21:11:40 -0400

    solved problem

--- python solutions/twoSum.py
+++
@@ -1,9 +0,0 @@
-def twoNumberSum(array, targetSum):
-    # Write your code here.
-	for i in range(len(array)):
-		firstNum = array[i]
-		for j in range(i + 1, len(array)):
-			secondNum = array[j]
-			if firstNum + secondNum == targetSum:
-				return [firstNum, secondNum]
-	return []
