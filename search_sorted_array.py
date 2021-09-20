ac8a37a1af2cde80fcb8316811e504aaf93da1ae
steventejeda <steventejeda@gmail.com>
2021-09-19 22:32:21 -0400

    solved problem

---
+++ search_sorted_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            return -1
+        # print(nums)
+        for i in range(len(nums)):
+            if target == nums[i]:
+                return i
