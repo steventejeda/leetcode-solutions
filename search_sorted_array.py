204cc64b508689f9661153c0587c6587abe53766
steventejeda <steventejeda@gmail.com>
2021-09-21 20:48:14 -0400

    solved problem

--- search_sorted_array.py
+++ search_sorted_array.py
@@ -1,8 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target not in nums:
-            return -1
-        # print(nums)
-        for i in range(len(nums)):
-            if target == nums[i]:
-                return i
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            middle = (left + right) // 2 
+            if target == nums[middle]:
+                return middle
+            
+            
+            if nums[left] <= nums[middle]:
+                if target > nums[middle] or target < nums[left]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+            else:
+                if target < nums[middle] or target > nums[right]:
+                    right = middle - 1
+                else: 
+                    left = middle + 1
+                    
+        return -1
+                
